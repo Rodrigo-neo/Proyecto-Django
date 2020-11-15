b0e0abd11027c54ec7d0fd47455e23f547859d0b
Rodrigo-neo <74388692+Rodrigo-neo@users.noreply.github.com>
2020-11-15 17:06:09 -0600

    Update settings.py

--- ProyectoFinal/ProyectoFinal/settings.py
+++ ProyectoFinal/ProyectoFinal/settings.py
@@ -102,6 +102,12 @@ AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
+    {
+        'NAME': 'django-advanced_password_validation.advanced_password_validation.ContainsSpecialCharactersValidator',
+        'OPTIONS': {
+            'min_characters': 1
+        }
+    },
 ]
 
 
